f9f66aaf31d127d93f261436c7992f52ffefa392
bisanjabareen <bisan.yousef25@gmail.com>
2026-06-10 23:45:44 +0300

    Add RISC-V instruction formats image and update CPU header and implementation for instruction handling

--- cpu/cpu.h
+++ cpu/cpu.h
@@ -1,22 +1,39 @@
 #include <cstdint>
 #include <vector>
+
+enum class Op {
+    ADD,
+    SUB,
+    ADDI,
+    LW,    
+    SW,    
+    BEQ,   
+    LUI,   
+};
+struct decoded_instruction_t {
+    uint8_t opcode;               
+    uint8_t rd;                   
+    uint8_t funct3;               
+    uint8_t rs1;                  
+    uint8_t rs2;                  
+    uint8_t funct7;
+    uint32_t imm;
+};
+
 class CPU {
 private:
-    // CPU registers
-    uint8_t A;  // Accumulator
-    uint8_t X;  // X index register
-    uint8_t Y;  // Y index register
-    uint8_t SP; // Stack pointer
-    uint16_t PC; // Program counter
-    uint8_t P;  // Processor status
-    std::vector<uint8_t> memory; // Memory
-    uint32_t registerFile[32]; // Cycle count
+    uint32_t PC;                    
+    std::vector<uint8_t> memory;    
+    uint32_t registerFile[32];      
+
 public:
     CPU();
     ~CPU();
-    void fetch();
-    void decode();
-    void execute();
-    void memory();
-    void run();
-}
+    void fetch(uint32_t* instruction);
+    void decode(uint32_t* instruction);
+    void execute(uint32_t* instruction);
+    void run(uint32_t* instruction);                     
+
+    uint8_t read_mem(uint32_t addr);
+    void write_mem(uint32_t addr, uint8_t value);
+};
